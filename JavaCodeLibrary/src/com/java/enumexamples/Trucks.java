5fff9abbd9f865406b5f4cf58ca91b6cf61e399f
Sahil Arora <sahil@Sahils-MacBook-Air.local>
2019-09-08 20:33:52 +0530

    Added comments

--- JavaCodeLibrary/src/com/java/enumexamples/Trucks.java
+++ JavaCodeLibrary/src/com/java/enumexamples/Trucks.java
@@ -1,4 +1,58 @@
 package com.java.enumexamples;
 
-public class Trucks {
+enum Trucks implements IVehicle
+{
+    MahindraStorm
+            {
+                @Override
+                public double speed(int miles, int hour) {
+                    System.out.println("MahindraStorm FIRST");
+                    return miles /(hour * 2);
+                }
+            },
+    TATAWARROR
+            {
+                @Override
+                public double speed(int miles, int hour) {
+                    System.out.println("TATAWARROR SECOND");
+
+                    return miles /(hour * 3);
+
+                }
+            },
+    PIAGGIORANGER
+            {
+                @Override
+                public double speed(int miles, int hour) {
+                    System.out.println("PIAGGIORANGER THIRD");
+                    return miles /(hour * 3);
+
+                }
+            };
+
+    @Override
+    public double speed(int miles, int hour) {
+        System.out.println("MAIN SPEED METHOD");
+        return miles/(hour);
+    }
+
+    @Override
+    public int mileage(int costOfFuel, int miles) {
+        System.out.println("MAIN MILEAGE METHOD");
+
+        return  miles / (costOfFuel * 10);
+    }
+}
+class MainClass {
+
+    public static void main(String args[])
+    {
+        // See Main speed method will never be executed
+        for(Trucks truck : Trucks.values())
+        {
+            System.out.println("Speed of " + truck.name()+ ": " + truck.speed(10000,30));  //Accessing enums directly using class name
+            System.out.println("Mileage of " + truck.name()+ ": " + truck.mileage(10,10000));
+        }
+    }
+
 }
